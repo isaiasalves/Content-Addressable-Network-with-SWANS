401f27e15ed99f20ce3476063a5c431b2ed0db8c
isaiasalves <isaias_alvess@hotmail.com>
2020-07-02 23:45:43 -0300

    Ajustes nos diretórios

---
+++ CSVGen/Starter/src/csvMaker/Pairs.java
@@ -0,0 +1,16 @@
+package csvMaker;
+
+import jist.minisim.*;
+import jist.swans.misc.Location;
+
+
+public class Pairs {
+	public String ipOrigem;
+	
+	public Location locationOrigem;
+	
+	public String ipDestino;
+	
+	public Location locationDestino;
+
+}
